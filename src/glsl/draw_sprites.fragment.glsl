a0aa028a5b3af0e41c80ad47d216616e8807a5cf
marffinn <marffinn@gmail.com>
2025-10-02 22:15:06 +0200

    git git git

---
+++ src/glsl/draw_sprites.fragment.glsl
@@ -0,0 +1,26 @@
+#version 430
+
+#ezquake-definitions
+
+layout(binding=0) uniform sampler2DArray materialTex;
+uniform bool alpha_test;
+
+in vec3 TextureCoord;
+in vec4 fsColor;
+
+out vec4 frag_color;
+
+void main()
+{
+	frag_color = texture(materialTex, TextureCoord);
+
+	frag_color *= fsColor;
+
+	if (alpha_test && frag_color.a < 0.3) {
+		discard;
+	}
+
+#ifdef DRAW_FOG
+	frag_color = applyFogBlend(frag_color, gl_FragCoord.z / gl_FragCoord.w);
+#endif
+}
